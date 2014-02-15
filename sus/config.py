c263ed2ee374ee182cd3f28a04032df176abc9b9
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-02-15 22:11:35 +0100

    first draft of relocations

--- sus/config.py
+++ sus/config.py
@@ -1,13 +1,15 @@
 """
-Config options, for the sole perpose of softcoding.
+Config options, for the sole purpose of soft-coding.
 The user should never have to alter this file.
 """
 
 SUS_EXTENSION = ".sus"
-SUS_PREFIX = "sus_"
 
-CONFIGURATIONS_FILE_NAME = SUS_PREFIX + "configurations" + SUS_EXTENSION
+CONFIGURATIONS_FILE_NAME = "configurations" + SUS_EXTENSION
+RELOCATIONS_FILE_NAME = "relocations" + SUS_EXTENSION
+
 SHARED_FOLDER_NAME = 'shared'
+RELOCS_FOLDER_NAME = 'relocations'
 
 DEFAULT_TRUE_COLOR = "green"
 DEFAULT_FALSE_COLOR = "red"
--- sus/relocation.py
+++ sus/relocation.py
@@ -1,9 +1,8 @@
 """
-SUS configuration
+SUS relocation
 """
 
 import os
-import socket
 
 import util
 import text_util
@@ -12,51 +11,38 @@ from deployment import Deployment
 import config as conf
 
 
-class Configuration(Deployment):
+class Relocation(Deployment):
     """
-    A class of configuration deployments.
-    Any instance of this class represents a deployment of exactly one config file.
+    A class of folder relocations.
+    Any instance of this class represents a relocation of exactly one folder.
     """
 
-    def __init__(self, depot, config_file_name, destination_path):
+    def __init__(self, depot, reloc_folder_name, destination_path):
         """
-        Initialize all information that is needed to perform and evaluate the deployment of this configuration.
+        Initialize all information that is needed to perform and evaluate this relocation.
         @param depot: The path to the depot folder.
-        @param config_file_name: The name of the config file to deploy.
-        @param destination_path: The path to the destination of the config file.
+        @param reloc_folder_name: The name of the folder to relocate.
+        @param destination_path: The path to the destination of the relocation folder
         """
         self.depot = depot
-        self.config_file_name = config_file_name
+        self.reloc_folder_name = reloc_folder_name
+        self.relocs_folder_path = os.path.join(depot, conf.RELOCS_FOLDER_NAME)
 
-        self.shared_folder_path = os.path.join(depot, conf.SHARED_FOLDER_NAME)
-        self.host_folder_path = os.path.join(depot, socket.gethostname())
-
-        self.config_in_shared_folder = os.path.join(self.shared_folder_path, self.config_file_name)
-        self.config_in_host_folder = os.path.join(self.host_folder_path, self.config_file_name)
-
-        self.shared_config_exists = os.path.exists(self.config_in_shared_folder)
-        self.host_config_exists = os.path.exists(self.config_in_host_folder)
-
-        self.source = None
-        if self.shared_config_exists:
-            self.source = self.config_in_shared_folder
-        if self.host_config_exists:
-            self.source = self.config_in_host_folder
+        self.source = os.path.join(self.relocs_folder_path, self.reloc_folder_name)
         self.destination = destination_path
-
 
     def deploy(self, args):
         """
-        Deploy this configuration.
+        Deploy this relocation.
         @param args: The parsed arguments from the command-line.
         """
         self.check_before()
-        self.link(args)
+        self.relocate(args)
         self.check_after()
 
     def check_before(self):
         """
-        Check the state of the configuration before deployment.
+        Check the state of the relocation before deployment.
         """
         self.src_exists = os.path.exists(self.source)
         self.src_is_file = os.path.isfile(self.source)
@@ -70,23 +56,23 @@ class Configuration(Deployment):
 
     def check_after(self):
         """
-        Check the state of the configuration after deployment.
+        Check the state of the relocation after deployment.
         """
         self.dst_exists_after = os.path.exists(self.destination)
         self.dst_is_file_after = os.path.isfile(self.destination)
         self.dst_is_link_after = os.path.islink(self.destination)
         self.dst_is_dir_after = os.path.isdir(self.destination)
 
-    def link(self, args):
+    def relocate(self, args):
         """
-        Actually deploy the configuration by linking the correct spot to the original file.
+        Actually relocate the folder by linking the correct spot to the original folder.
         @param args: The parsed arguments from the command-line.
         """
         if args.dry:
             print(str(self.source) + " -> " + str(self.destination))
             return
 
-        if not self.src_exists:
+        if not self.src_exists or self.src_is_file:
             return
 
         if self.dst_exists_before:
@@ -129,7 +115,6 @@ class Configuration(Deployment):
             return result[1:]
 
         blocks = list()
-        blocks.append(self.src_exists)
         blocks.append(self.dst_exists_after)
         blocks.append(
             self.src_is_dir == self.dst_is_dir_after and self.src_is_file == self.dst_is_file_after)
